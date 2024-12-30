449dafe2d6ff9608f4980df69534af18e1e4659e
silpakammara99 <silpakammara9702@gmail.com>
2024-12-30 11:42:08 +0530

    place-order, cart-total and etc changes

--- Food-Delivery-APP/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Food-Delivery-APP/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,12 +1,55 @@
+import { StoreContext } from '../../context/StoreContext'
 import './PlaceOrder.css'
 
-import React from 'react'
+import React, { useContext } from 'react'
 
 const PlaceOrder = () => {
+const {getTotalCartAmount}=useContext(StoreContext)
+
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">
+          Delivery Information
+        </p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="text" placeholder='email address'/>
+        <input type="text" placeholder='street'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='city' />
+          <input type="text" placeholder='state' />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip code' />
+          <input type="text" placeholder='country' />
+        </div>
+        <input type='text' placeholder='phone' ></input>
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+      <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr/>
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr/>
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+           
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+
+      </div>
+    </form>
   )
 }
 
